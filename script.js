d85133cf385d992442bf496ad8b3a1acac835306
Buddhikanip <98378758+Buddhikanip@users.noreply.github.com>
2022-02-01 12:07:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,442 @@
+function m() {
+    var x = document.getElementById("summary");
+    x.className = "free";
+}
+
+function n() {
+    var x = document.getElementById("scorecard");
+    x.className = "active";
+}
+
+function o() {
+    var x = document.getElementById("scorecard");
+    x.className = "free";
+}
+
+function p() {
+    var x = document.getElementById("summary");
+    x.className = "active";
+}
+
+
+
+function s() {
+    var x = document.getElementById("bbulls");
+    x.className = "free";
+}
+
+function q() {
+    var x = document.getElementById("bbulls");
+    x.className = "active";
+}
+
+function r() {
+    var x = document.getElementById("swar");
+    x.className = "free";
+}
+
+function t() {
+    var x = document.getElementById("swar");
+    x.className = "active";
+}
+
+
+
+
+
+
+function iWarriorsInstarVisit() {
+    window.open("https://www.instagram.com/p/CZOEtYXoTcR/?utm_medium=share_sheet , _blank");
+}
+
+function mediInstarVisit() {
+    window.open("https://www.instagram.com/p/CZOFEWZItGI/?utm_medium=share_sheet , _blank");
+}
+
+function statInstarVisit() {
+    window.open("https://www.instagram.com/p/CZOFJ4FId7n/?utm_medium=share_sheet , _blank");
+}
+
+function strikersInstarVisit() {
+    window.open("https://www.instagram.com/p/CZOE_1aIfit/?utm_medium=share_sheet , _blank");
+}
+
+function thundersInstarVisit() {
+    window.open("https://www.instagram.com/mat_ct_22/p/CZOEkRSoYiv/?utm_medium=share_sheet , _blank");
+}
+
+function bullsInstarVisit() {
+    window.open("https://www.instagram.com/p/CZOEh60IzV3/?utm_medium=share_sheet , _blank");
+}
+
+function iWarriors() {
+    window.location = "iWarriors.html";
+}
+
+function medi() {
+    window.location = "medi.html";
+}
+
+function strikers() {
+    window.location = "strikers.html";
+}
+
+function bulls() {
+    window.location = "bulls.html";
+}
+
+function stat() {
+    window.location = "stat.html";
+}
+
+function thunders() {
+    window.location = "thunders.html";
+}
+
+function matchSummary() {
+    window.location = "summary.html";
+}
+
+function pageReload() {
+
+    var x = window.location = "index.html";
+    x.locatiion.reload();
+
+}
+
+function pageReloadStart() {
+    setInterval(pageReload, 30000);
+
+}
+
+function m1() {
+    window.location = "m1.html";
+}
+
+function m2() {
+    window.location = "m2.html";
+}
+
+function m3() {
+    window.location = "m3.html";
+}
+
+function m4() {
+    window.location = "m4.html";
+}
+
+function m5() {
+    window.location = "m5.html";
+}
+
+function m6() {
+    window.location = "m6.html";
+}
+
+function mF() {
+    window.location = "f.html";
+}
+
+function fb() {
+    window.open("https://www.facebook.com/MAT-Challenge-Trophy-103952355533021/", "_blank");
+}
+
+function ins() {
+    window.open("https://instagram.com/mat_ct_22?utm_medium=copy_link", "_blank");
+}
+function insLogo(){
+    window.open("https://instagram.com/mat_ct_22?utm_medium=copy_link", "_blank");
+}
+function pt(){
+    window.open("p.html", "_blank");
+}
+
+
+
+
+
+var score = document.getElementById("score");
+var overs = document.getElementById("overs");
+
+
+
+var totalScore = 0;
+var wickets = 0;
+var totalBalls = 0;
+var totalOvers = 0;
+
+
+
+function scoreboard(){
+    window.location = "m1.html" ;
+}
+
+
+
+
+
+function match() {
+    score.innerHTML = totalScore + "/" + wickets;
+    overs.innerHTML = "(" + totalOvers +"."+totalBalls+ ")";
+    
+}
+
+function match1() {
+    var score1 = document.getElementById("score1");
+    var t1Overs=document.getElementById("t1Overs");
+    score1.innerHTML = totalScore + "/" + wickets;
+    t1Overs.innerHTML="(" + totalOvers +"."+totalBalls+ ")";
+}
+
+function match2() {
+    var score1 = document.getElementById("score1");
+    score1.innerHTML = totalScore + "/" + wickets;
+    var t1Overs=document.getElementById("t1Overs");
+    t1Overs.innerHTML="(" + totalOvers +"."+totalBalls+ ")";
+}
+
+function match3() {
+    var score1 = document.getElementById("score1");
+    score1.innerHTML = totalScore + "/" + wickets;
+    var t1Overs=document.getElementById("t1Overs");
+    t1Overs.innerHTML="(" + totalOvers +"."+totalBalls+ ")";
+}
+
+function match4() {
+    var score1 = document.getElementById("score1");
+    score1.innerHTML = totalScore + "/" + wickets;
+    var t1Overs=document.getElementById("t1Overs");
+    t1Overs.innerHTML="(" + totalOvers +"."+totalBalls+ ")";
+}
+
+function match5() {
+    var score1 = document.getElementById("score1");
+    score1.innerHTML = totalScore + "/" + wickets;
+    var t1Overs=document.getElementById("t1Overs");
+    t1Overs.innerHTML="(" + totalOvers +"."+totalBalls+ ")";
+}
+
+function match6() {
+    var score1 = document.getElementById("score1");
+    score1.innerHTML = totalScore + "/" + wickets;
+    var t1Overs=document.getElementById("t1Overs");
+    t1Overs.innerHTML="(" + totalOvers +"."+totalBalls+ ")";
+}
+
+function matchFinal() {
+    var score1 = document.getElementById("score1");
+    score1.innerHTML = totalScore + "/" + wickets;
+    var t1Overs=document.getElementById("t1Overs");
+    t1Overs.innerHTML="(" + totalOvers +"."+totalBalls+ ")";
+}
+
+
+
+function m() {
+    var x = document.getElementById("summary");
+    x.className = "free";
+}
+
+function n() {
+    var x = document.getElementById("scorecard");
+    x.className = "active";
+}
+
+function o() {
+    var x = document.getElementById("scorecard");
+    x.className = "free";
+}
+
+function p() {
+    var x = document.getElementById("summary");
+    x.className = "active";
+}
+
+
+
+function s() {
+    var x = document.getElementById("team1");
+    x.className = "free";
+}
+
+function q() {
+    var x = document.getElementById("team1");
+    x.className = "active";
+}
+
+function r() {
+    var x = document.getElementById("team2");
+    x.className = "free";
+}
+
+function t() {
+    var x = document.getElementById("team2");
+    x.className = "active";
+}
+
+
+//match1 bulls vs stat
+
+/*var scoreBoard = 0;
+
+
+var bullsRuns = 0;
+var bullsTotalRuns = 0;
+var bullsTotalBalls = 0;
+var bullsOvers = 0;
+var bullsTotalOvers = 0;
+var bullsOvers = 0;
+var extras = 0;
+
+
+var kavishkaRuns;
+var kavishkaBalls;
+var nipulRuns;
+var nipulBalls;
+var bhanukaRuns;
+var bhanukaBalls;
+var bimsaraRuns;
+var bimsaraBalls;
+var sasankaRuns;
+var sasankaBalls;
+var lasithRuns;
+var lasithBalls;
+var sanuthRuns;
+var sanuthBalls;
+var lakruwanRuns;
+var lakruwanBalls;
+var lumbiniRuns;
+var lumbiniBalls;
+var chathuniRuns;
+var chathuniBalls;
+var mashaRuns;
+var mashaBalls;
+
+var kavishka = 10;
+var kavishkaB = 90;
+
+var nipul = 20;
+var nipulB = 80;
+
+var bhanuka = 30;
+var bhanukaB = 70;
+
+var bimsara = 40;
+var bimsaraB = 60;
+
+var sasanka = 50;
+var sasankaB = 50;
+
+var lasith = 60;
+var lasithB = 40;
+
+var sanuth = 70;
+var sanuthB = 30;
+
+var lakruwan = 80;
+var lakruwanB = 20;
+
+var lumbini = 90;
+var lumbiniB = 10;
+
+var chathuni = 50;
+var chathuniB = 11;
+
+var masha = 50;
+var mashaB = 12;
+
+var extraRuns = 20;
+
+function match1() {
+
+
+    bullsRuns = document.getElementById("bullsRuns");
+    kavishkaRuns = document.getElementById("kavishkaRuns");
+    kavishkaBalls = document.getElementById("kavishkaBalls");
+    nipulRuns = document.getElementById("nipulRuns");
+    nipulBalls = document.getElementById("nipulBalls");
+    bhanukaRuns = document.getElementById("bhanukaRuns");
+    bhanukaBalls = document.getElementById("bhanukaBalls");
+    mashaRuns = document.getElementById("mashaRuns");
+    mashaBalls = document.getElementById("mashaBalls");
+    lumbiniRuns = document.getElementById("lumbiniRuns");
+    lumbiniBalls = document.getElementById("lumbiniBalls");
+    sanuthRuns = document.getElementById("sanuthRuns");
+    sanuthBalls = document.getElementById("sanuthBalls");
+    lasithRuns = document.getElementById("lasithRuns");
+    lasithBalls = document.getElementById("lasithBalls");
+    sasankaRuns = document.getElementById("sasankaRuns");
+    sasankaBalls = document.getElementById("sasankaBalls");
+    sasankaRuns = document.getElementById("sasankaRuns");
+    sasankaBalls = document.getElementById("sasankaBalls");
+    chathuniRuns = document.getElementById("chathuniRuns");
+    chathuniBalls = document.getElementById("chathuniBalls");
+    lakruwanRuns = document.getElementById("lakruwanRuns");
+    lakruwanBalls = document.getElementById("lakruwanBalls");
+    bimsaraRuns = document.getElementById("bimsaraRuns");
+    bimsaraBalls = document.getElementById("bimsaraBalls");
+
+    bullsOvers = document.getElementById("bullsOvers");
+    extras = document.getElementById("extras");
+
+    bullsTotalBalls = kavishkaB + nipulB + lasithB + lumbiniB + sanuthB + sasankaB + bimsaraB + bhanukaB + chathuniB + mashaB + lakruwanB;
+
+
+    bullsTotalRuns = kavishka + masha + lumbini + sanuth + lasith + sasanka + sasanka + bhanuka + nipul + chathuni + lakruwan + extraRuns;
+
+
+    kavishkaRuns.innerHTML = kavishka;
+    kavishkaBalls.innerHTML = kavishkaB;
+    nipulRuns.innerHTML = nipul;
+    nipulBalls.innerHTML = nipulB;
+    lasithRuns.innerHTML = lasith;
+    lasithBalls.innerHTML = lasithB;
+    lumbiniRuns.innerHTML = lumbini;
+    lumbiniBalls.innerHTML = lumbiniB;
+    sanuthRuns.innerHTML = sanuth;
+    sanuthBalls.innerHTML = sanuthB;
+    sasankaRuns.innerHTML = sasanka;
+    sasankaBalls.innerHTML = sasankaB;
+    bimsaraRuns.innerHTML = bimsara;
+    bimsaraBalls.innerHTML = bimsaraB;
+    bhanukaRuns.innerHTML = bhanuka;
+    bhanukaBalls.innerHTML = bhanukaB;
+    chathuniRuns.innerHTML = chathuni;
+    chathuniBalls.innerHTML = chathuniB;
+    mashaRuns.innerHTML = masha;
+    mashaBalls.innerHTML = mashaB;
+    lakruwanRuns.innerHTML = lakruwan;
+    lakruwanBalls.innerHTML = lakruwanB;
+
+    bullsRuns.innerHTML = bullsTotalRuns;
+    scoreBoard.innerHTML = bullsTotalRuns;
+    extras.innerHTML = extraRuns;
+}
+
+
+var a = 1;
+var bullsBalls = 0;
+bullsTotalOvers = 0;
+
+function x(event) {
+    if (event.key == "1") {
+        a = 0;
+    }
+}
+
+function overs() {
+    if (a == 0) {
+        if (bullsBalls < 5) {
+            bullsBalls = bullsBalls + 1;
+            bullsOvers.innerHTML = "(" + bullsTotalOvers + "." + bullsBalls + ")";
+            a = 1;
+        } else {
+            bullsBalls = -1;
+            bullsTotalOvers = bullsTotalOvers + 1;
+        }
+    }
+}
+
+function oversCal() {
+    setInterval(overs, 100);
+}*/
